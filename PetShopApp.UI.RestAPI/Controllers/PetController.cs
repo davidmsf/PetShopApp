116e91cab80cb07c0999f9eed6ec14057434142b
davidmsf <davidmsf@gmail.com>
2019-09-03 09:46:58 +0200

    restapi

---
+++ PetShopApp.UI.RestAPI/Controllers/PetController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetShopApp.UI.RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetController : ControllerBase
+    {
+
+    }
+}
